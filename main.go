85b4952f56318e7d08c653a96250292c3fa2e3de
Garvit Sadhwani <garvit.sadh@gmail.com>
2022-08-12 00:53:35 +0530

    basic functionality

--- main.go
+++ main.go
@@ -14,6 +14,11 @@ func main() {
 	router := chi.NewRouter()
 	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml"))
 	router.Get("/", controllers.StaticHandler(tpl))
+
+	usersC := controllers.Users{}
+	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml"))
+	router.Get("/signup", controllers.StaticHandler(usersC.Templates.New))
+
 	fmt.Printf("starting server at 8080")
 	http.ListenAndServe(":8080", router)
 
