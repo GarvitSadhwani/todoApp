2ca0afd113a5d2299d761be6f1422b892f639937
Garvit Sadhwani <garvit.sadh@gmail.com>
2022-08-11 21:41:20 +0530

    added MVC

--- main.go
+++ main.go
@@ -2,30 +2,21 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 	"path/filepath"
 
+	"github.com/GarvitSadhwani/todoApp/controllers"
 	"github.com/GarvitSadhwani/todoApp/views"
 	chi "github.com/go-chi/chi/v5"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "home.gohtml")
-	tpl, err := template.ParseFiles(tplPath)
+func main() {
+	router := chi.NewRouter()
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
 	if err != nil {
 		fmt.Printf("error parsing")
 	}
-	viewTpl := views.Template{
-		HTMLTpl: tpl,
-	}
-	viewTpl.Execute(w, nil)
-
-}
-
-func main() {
-	router := chi.NewRouter()
-	router.Get("/", homeHandler)
+	router.Get("/", controllers.StaticHandler(tpl))
 	fmt.Printf("starting server at 8080")
 	http.ListenAndServe(":8080", router)
 
--- views/template.go
+++ views/template.go
@@ -10,6 +10,16 @@ type Template struct {
 	HTMLTpl *template.Template
 }
 
+func Parse(filepath string) (Template, error) {
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		fmt.Printf("Error parsing")
+	}
+	return Template{
+		HTMLTpl: tpl,
+	}, nil
+}
+
 func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	err := t.HTMLTpl.Execute(w, data)
